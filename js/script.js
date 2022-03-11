696217cd58e6b297f2afba587b85d79da9d20d0f
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-11 14:55:54 +0100

    Added base thumbs

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/* Partendo dai file nello starter implementare la funzione dei bottoni (next e previous) dello slider usando Vue (prima però create la struttura dati). */
+
+// creo la struttura
+const app = new Vue({
+    el: "#root",
+    data:{
+        activeIndex: 0,
+        arrSlides: [
+            {
+                title: "Svezia",
+                img: "01.jpg",
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat cumque quos omnis aliquam perferendis numquam unde, id iure odit enim deleniti ex soluta velit, commodi eius consequatur quas! Doloribus, ipsa!",
+            },
+            {
+                title: "Svizzera",
+                img: "02.jpg",
+                text: "Lorem ipsum",
+            },
+            {
+                title: "Gran Bretagna",
+                img: "03.jpg",
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            },
+            {
+                title: "Germania",
+                img: "04.jpg",
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat cumque quos omnis aliquam perferendis numquam unde, id iure odit enim deleniti ex soluta velit.",
+            },
+            {
+                title: "Paradise",
+                img: "05.jpg",
+                text: "Commodi eius consequatur quas.",
+            },
+        ]
+    },
+    
+})
